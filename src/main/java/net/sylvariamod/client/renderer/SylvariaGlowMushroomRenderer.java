d38517082bd9e5d43d694f3fe42e222eb0a153d2
kzstkapafar-web <kz.st.kapafar@gmail.com>
2026-08-06 02:57:04 +0300

    Update SylvariaGlowMushroomRenderer.java

--- src/main/java/net/sylvariamod/client/renderer/SylvariaGlowMushroomRenderer.java
+++ src/main/java/net/sylvariamod/client/renderer/SylvariaGlowMushroomRenderer.java
@@ -35,7 +35,7 @@ public class SylvariaGlowMushroomRenderer implements BlockEntityRenderer<Sylvari
 
         BlockState state = be.getBlockState();
         BakedModel emissiveModel = Minecraft.getInstance().getModelManager()
-                .getModel(new ModelResourceLocation(EMISSIVE_MODEL_LOC, ""));
+                .getModel(ModelResourceLocation.standalone(EMISSIVE_MODEL_LOC));
 
         if (emissiveModel == null) {
             return;
